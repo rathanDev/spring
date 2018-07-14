cc7c2e11584aa4ef72c84a8ddf0dddcec9d3b835
Jana <rathan.kparam@gmail.com>
2018-07-14 16:01:17 +0530

    Add controller

---
+++ spring-hibernate/src/main/java/jana/controller/HomeController.java
@@ -0,0 +1,14 @@
+package jana.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greeting from spring boot hibernate";
+    }
+
+}
