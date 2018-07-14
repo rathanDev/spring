ee970397aff407c95b5c4a07a79feab261ccee22
Jana <rathan.kparam@gmail.com>
2018-07-14 14:49:06 +0530

    Inspect beans created

--- spring-boot-service/src/main/java/org/jana/App.java
+++ spring-boot-service/src/main/java/org/jana/App.java
@@ -1,13 +1,30 @@
 package org.jana;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class App {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Inspecting beans provided by spring boot");
+            String[] names = ctx.getBeanDefinitionNames();
+            Arrays.sort(names);
+            for(String name: names) {
+                System.out.println("name = " + name);
+            }
+        };
     }
 
 }
