42062200455aaf39e9130101ae817518a1b7d0f7
Jana <rathan.kparam@gmail.com>
2018-07-14 16:34:09 +0530

    Create table workplace

--- spring-hibernate/db.sql
+++ spring-hibernate/db.sql
@@ -3,8 +3,13 @@ create database hibernate;
 use hibernate;
 
 create table employee (
- id int(3) unsigned not null auto_increment,
+ id int(3) unsigned not null auto_increment primary key,
  name varchar(30),
- workplace int(3),
- primary key (id)
+ workplace int(3)
 );
+
+create table workplace (
+  id int(3) unsigned not null auto_increment primary key,
+  name varchar(30)
+);
+-- drop table workplace;
