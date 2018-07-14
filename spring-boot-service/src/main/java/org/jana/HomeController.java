e428ff9dc469bf22cf7d55500a521d0eb4d4856a
Jana <rathan.kparam@gmail.com>
2018-07-14 14:43:18 +0530

    Add home controller

---
+++ spring-boot-service/src/main/java/org/jana/HomeController.java
@@ -0,0 +1,14 @@
+package org.jana;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring boot";
+    }
+
+}
