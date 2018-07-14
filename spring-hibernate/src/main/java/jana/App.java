ce05d9bce2ab1b91888b1cab6a9fb49c4f0ef80e
Jana <rathan.kparam@gmail.com>
2018-07-14 16:09:04 +0530

    Add SpringBootApplication starting point

--- spring-hibernate/src/main/java/jana/App.java
+++ spring-hibernate/src/main/java/jana/App.java
@@ -1,9 +1,30 @@
 package jana;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Beans created by spring boot --->");
+            String[] names = ctx.getBeanDefinitionNames();
+            Arrays.sort(names);
+            for(String name: names) {
+                System.out.println("name = " + name);
+            }
+        };
     }
 
 }
