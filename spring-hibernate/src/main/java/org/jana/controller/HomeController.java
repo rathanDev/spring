5de205f3c732f6097b4c7e2fa60ebf6f634c134e
Jana <rathan.kparam@gmail.com>
2018-07-16 00:18:55 +0530

    Wip: Query employees

--- spring-hibernate/src/main/java/org/jana/controller/HomeController.java
+++ spring-hibernate/src/main/java/org/jana/controller/HomeController.java
@@ -1,14 +1,28 @@
 package org.jana.controller;
 
+import org.jana.model.Employee;
+import org.jana.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HomeController {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     @RequestMapping("/")
     public String index() {
         return "Greeting from spring boot hibernate";
+    }
+
+    @RequestMapping("/query")
+    public List<Employee> query() {
+        System.out.println("employeeRepository = " + employeeRepository);
+        return employeeRepository.findAll();
     }
 
 }
