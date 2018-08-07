d12986c3f9622a4957e714daf55048ba44d1ca24
janarthan <janarthan@finnovation.lk>
2018-08-07 14:44:01 +0530

    Add Greeting Controller

---
+++ web-socket/src/main/java/org/jana/websocket/GreetingController.java
@@ -0,0 +1,11 @@
+package org.jana.websocket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.me
+
+@Controller
+public class GreetingController {
+
+    @Message
+
+}
