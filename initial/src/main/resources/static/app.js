0836e0ca1ab37096a169fe3ca91d8de3c90d753e
janarthan <janarthan@finnovation.lk>
2018-08-07 16:01:52 +0530

    Format code

--- initial/src/main/resources/static/app.js
+++ initial/src/main/resources/static/app.js
@@ -44,7 +44,13 @@ $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+    $("#connect").click(function () {
+        connect();
+    });
+    $("#disconnect").click(function () {
+        disconnect();
+    });
+    $("#send").click(function () {
+        sendName();
+    });
 });
