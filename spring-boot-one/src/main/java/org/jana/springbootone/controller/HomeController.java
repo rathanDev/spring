fce3e43ece962626b34d367bcae30c29f1fd6429
janarthan <janarthan@finnovation.lk>
2018-08-08 12:47:09 +0530

    Init spring-boot app

---
+++ spring-boot-one/src/main/java/org/jana/springbootone/controller/HomeController.java
@@ -0,0 +1,14 @@
+package org.jana.springbootone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greeting from this boot!";
+    }
+
+}
