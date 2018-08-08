cca67d2353495ca1284b228b3559f907c284e354
janarthan <janarthan@finnovation.lk>
2018-08-08 14:22:23 +0530

    Add swagger annotations

--- spring-boot-one/src/main/java/org/jana/springbootone/controller/PetController.java
+++ spring-boot-one/src/main/java/org/jana/springbootone/controller/PetController.java
@@ -1,6 +1,9 @@
 package org.jana.springbootone.controller;
 
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.jana.springbootone.model.Pet;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,9 +34,16 @@ public class PetController {
         return ResponseEntity.ok().body(pet);
     }
 
+    @ApiOperation(notes = "Returns a pet when 0 < ID <= 10.  ID > 10 or non-integers will simulate API error conditions", value = "Find pet by ID", nickname = "getPetById",
+            tags = {"Pets"} )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Nice!", response = Pet.class),
+            @ApiResponse(code = 400, message = "Invalid ID supplied", response = Pet.class),
+            @ApiResponse(code = 404, message = "Pet not found", response = Pet.class)
+    })
     @RequestMapping(value = "/swagger/{id}", method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<Pet> useSwagger(@ApiParam(value = "ID of pet that needs to be fetched", allowableValues = "range[1,10]", required = true) @PathVariable("id") Integer petId) {
-        System.out.println("get pet " + petId);
+        System.out.println("get pet 1421 " + petId);
         Pet pet = new Pet();
         pet.setId(petId);
         return ResponseEntity.ok().body(pet);
