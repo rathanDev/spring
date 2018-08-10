c169ffc63ebb082ad739386b86cf9d386f1a1dfa
janarthan <janarthan@finnovation.lk>
2018-08-10 11:01:05 +0530

    Init micor service

---
+++ micro/db-service/src/main/java/org/jana/dbservice/repository/QuotesRepository.java
@@ -0,0 +1,4 @@
+package org.jana.dbservice.repository;
+
+public class QuotesRepository {
+}
---
+++ micro/db-service/src/main/java/org/jana/dbservice/resource/DbServiceResource.java
@@ -0,0 +1,11 @@
+package org.jana.dbservice.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/db")
+public class DbServiceResource {
+
+
+}
