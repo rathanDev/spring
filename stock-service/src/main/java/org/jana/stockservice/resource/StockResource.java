2d25150718873c511221e20f672af1cb8422ce00
janarthan <janarthan@finnovation.lk>
2018-08-10 15:33:48 +0530

    Update getStock()

--- stock-service/src/main/java/org/jana/stockservice/resource/StockResource.java
+++ stock-service/src/main/java/org/jana/stockservice/resource/StockResource.java
@@ -1,14 +1,20 @@
 package org.jana.stockservice.resource;
 
-import org.jana.stockservice.model.StockQuote;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/rest/stock")
@@ -17,11 +23,33 @@ public class StockResource {
     @Autowired
     RestTemplate restTemplate;
 
-    @GetMapping("/{username}")
-    public List<StockQuote> getStock(@PathVariable("username") final String username) {
-        System.out.println("Get stock");
-        List<String> quotes = restTemplate.getForObject("http://localhost:8300/rest/db" + username, List.class);
+    /*
+    List<String> quotes = restTemplate.getForObject("http://localhost:8300/rest/db" + username, List.class);
+     @Override
+                    public Type getType() {
+                        return super.getType();
+                    }
+    * */
 
-        return null;
+    @GetMapping("/{username}")
+    public List<Stock> getStock(@PathVariable("username") final String username) {
+        System.out.println("Get stock");
+        ResponseEntity<List<String>> quoteResponse = restTemplate.exchange("http://localhost:8300/rest/db" + username, HttpMethod.GET,
+                null, new ParameterizedTypeReference<List<String>>() {
+                });
+        List<String> quotes = quoteResponse.getBody();
+        return quotes.stream()
+                .map(this::getStockPrice)
+                .collect(Collectors.toList());
     }
+
+    private Stock getStockPrice(String quote) {
+        try {
+            return YahooFinance.get(quote);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
