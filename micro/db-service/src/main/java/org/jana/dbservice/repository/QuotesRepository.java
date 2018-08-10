8b16320e8423529be6567d6f767def8268c64452
janarthan <janarthan@finnovation.lk>
2018-08-10 11:24:56 +0530

    Init

--- micro/db-service/src/main/java/org/jana/dbservice/repository/QuotesRepository.java
+++ micro/db-service/src/main/java/org/jana/dbservice/repository/QuotesRepository.java
@@ -1,4 +1,12 @@
 package org.jana.dbservice.repository;
 
-public class QuotesRepository {
+import org.jana.dbservice.model.Quote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+
+    List<Quote> findByUsername(String username);
+
 }
