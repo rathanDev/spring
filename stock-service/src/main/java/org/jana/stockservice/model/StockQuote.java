366520619e735b8e6e0c1f6b8fc3d8e7bf84b9e9
janarthan <janarthan@finnovation.lk>
2018-08-13 12:57:13 +0530

    Add StockQuote model

--- stock-service/src/main/java/org/jana/stockservice/model/StockQuote.java
+++ stock-service/src/main/java/org/jana/stockservice/model/StockQuote.java
@@ -1,4 +1,22 @@
 package org.jana.stockservice.model;
 
 public class StockQuote {
+
+    private String name;
+
+    public StockQuote() {
+    }
+
+    public StockQuote(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
