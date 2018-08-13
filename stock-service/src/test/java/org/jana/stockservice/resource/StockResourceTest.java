4738fb3e546668f9fa60d56b072d2a6cf6d73c1b
janarthan <janarthan@finnovation.lk>
2018-08-13 13:45:19 +0530

    Wip: Add unit test for get api

--- stock-service/src/test/java/org/jana/stockservice/resource/StockResourceTest.java
+++ stock-service/src/test/java/org/jana/stockservice/resource/StockResourceTest.java
@@ -1,21 +1,12 @@
 package org.jana.stockservice.resource;
 
-import org.jana.stockservice.model.StockQuote;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,13 +17,8 @@ public class StockResourceTest {
 
     @Test
     public void testGet() {
-        System.out.println("Get stock");
-//        ResponseEntity<List<String>> quoteResponse = restTemplate.exchange("http://localhost:8300/rest/db", HttpMethod.GET,
-//                null, new ParameterizedTypeReference<List<String>>() {
-//                });
-//        System.out.println("quoteResponse = " + quoteResponse);
-
-        ResponseEntity<String> call= restTemplate.getForEntity("http://localhost:8301/rest/stock/get-string", String.class);
+        System.out.println("Get sample string");
+        ResponseEntity<String> call= restTemplate.getForEntity("http://localhost:8301/rest/stock/sample-string", String.class);
         System.out.println(call.getBody());
     }
 
