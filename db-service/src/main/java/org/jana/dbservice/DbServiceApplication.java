bc7adf20200ff3d5fd7254c01def838286461d48
janarthan <janarthan@finnovation.lk>
2018-08-13 15:45:11 +0530

    Enable EurekaClient to db service=

--- db-service/src/main/java/org/jana/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/org/jana/dbservice/DbServiceApplication.java
@@ -2,8 +2,10 @@ package org.jana.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "org.jana.dbservice.repository")
 @SpringBootApplication
 public class DbServiceApplication {
