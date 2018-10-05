f0afbfd24a469a29e3ff78fac3ee3ba73a9dc85a
jana <rathan.kparam@gmail.com>
2018-10-05 20:01:50 +0530

    Configure jsp

--- ticketing/src/main/java/org/sliit/controller/HomeController.java
+++ ticketing/src/main/java/org/sliit/controller/HomeController.java
@@ -1,17 +1,23 @@
 package org.sliit.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("")
-    public String login(Model model) {
-        System.out.println("login");
-        model.addAttribute("name", "user");
-        return "greeting";
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "welcome";
     }
 
 }
