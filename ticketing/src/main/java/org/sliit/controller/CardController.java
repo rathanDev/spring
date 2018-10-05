99aa3f6bd1478cee22332e41e97165197b3e34a6
jana <rathan.kparam@gmail.com>
2018-10-05 07:39:36 +0530

    Organize

---
+++ ticketing/src/main/java/org/sliit/controller/CardController.java
@@ -0,0 +1,10 @@
+package org.sliit.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CardController {
+
+
+
+}
--- ticketing/src/main/java/org/sliit/controller/TicketController.java
+++ ticketing/src/main/java/org/sliit/controller/TicketController.java
@@ -1,4 +1,4 @@
-package org.sliit;
+package org.sliit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
