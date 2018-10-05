6154b7d71d0dd7e7a39768ba4d1d4d6948932e76
jana <rathan.kparam@gmail.com>
2018-10-05 07:44:39 +0530

    Add card

--- ticketing/src/main/java/org/sliit/controller/CardController.java
+++ ticketing/src/main/java/org/sliit/controller/CardController.java
@@ -1,10 +1,17 @@
 package org.sliit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CardController {
 
-
+    @GetMapping("/card")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "card";
+    }
 
 }
