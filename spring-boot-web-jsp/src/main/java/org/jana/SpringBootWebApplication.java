599b27315c0122edd8bf4ec1aadfa65eedbd72b8
Jana <janarthan.paramanandam@ge.com>
2019-03-16 18:25:25 +0530

    Update spring version

--- spring-boot-web-jsp/src/main/java/org/jana/SpringBootWebApplication.java
+++ spring-boot-web-jsp/src/main/java/org/jana/SpringBootWebApplication.java
@@ -2,18 +2,11 @@ package org.jana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+public class SpringBootWebApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
-	}
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
