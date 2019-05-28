ab34b15602cb43051bc03b960c20b082af827432
Jana <janarthan.paramanandam@ge.com>
2019-05-28 17:40:31 +0530

    Add readFromQueue()

--- aws/src/main/java/org/jana/aws/SQSListener.java
+++ aws/src/main/java/org/jana/aws/SQSListener.java
@@ -7,22 +7,19 @@ import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.AmazonSQSException;
 import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+
+import java.util.List;
 
 public class SQSListener {
 
     static final String ACCESS_KEY = "AKIARTYJ4YQ3PVGQARHM";
     static final String SECRET_KEY = "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";
     static final String REGION = "us-east-1";
+    static final String QUEUE = "firstQueue";
 
     public static void main(String[] args) {
-
-        SQSListener sqsListener = new SQSListener();
-        sqsListener.createQueue();
-
-    }
-
-    public void createQueue() {
-        //        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
 
         AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
         AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(credentials);
@@ -32,17 +29,35 @@ public class SQSListener {
                 .withCredentials(awsStaticCredentialsProvider)
                 .build();
 
-        CreateQueueRequest create_request = new CreateQueueRequest("thirdQueue")
+        SQSListener sqsListener = new SQSListener();
+        sqsListener.createQueue(amazonSQS);
+        sqsListener.readFromQueue(amazonSQS);
+
+    }
+
+    public void createQueue(AmazonSQS sqs) {
+        //        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+        CreateQueueRequest create_request = new CreateQueueRequest("fourthQueue")
                 .addAttributesEntry("DelaySeconds", "60")
                 .addAttributesEntry("MessageRetentionPeriod", "86400");
 
         try {
-            amazonSQS.createQueue(create_request);
+            sqs.createQueue(create_request);
         } catch (AmazonSQSException e) {
             if (!e.getErrorCode().equals("QueueAlreadyExists")) {
                 throw e;
             }
         }
+    }
+
+    public void readFromQueue(AmazonSQS sqs) {
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(QUEUE)
+                .withWaitTimeSeconds(10)
+                .withMaxNumberOfMessages(10);
+        List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+
+        System.out.println("message = " + messages.get(0));
     }
 
 }
